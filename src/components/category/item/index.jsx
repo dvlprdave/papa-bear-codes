3eb3483353af5075992ea00adc0c622c184293c2
David <devdavee@gmail.com>
2020-08-24 11:14:12 -0400

    added dynamic tag colors

--- src/components/category/item/index.jsx
+++ src/components/category/item/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-export const Item = ({ title, category, selectCategory }) => (
-  <li
-    className="item"
-    role="tab"
-    aria-selected={category === title ? 'true' : 'false'}
-  >
-    <div onClick={() => selectCategory(title)}>{title}</div>
-  </li>
-)
+export const Item = ({ title, category, selectCategory }) => {
+  const colorClasses =
+    title === 'React'
+      ? 'color-react'
+      : title === 'JavaScript'
+      ? 'color-javascript'
+      : null
+
+  return (
+    <li
+      className="item"
+      role="tab"
+      aria-selected={category === title ? 'true' : 'false'}
+    >
+      <div className={colorClasses} onClick={() => selectCategory(title)}>
+        {title}
+      </div>
+    </li>
+  )
+}
