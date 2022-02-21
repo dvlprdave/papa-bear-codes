43df62709c940225e8cc3148e130629618e397a6
David <devdavee@gmail.com>
2022-02-21 13:34:23 -0600

    Intro state change

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Web development blog`,
   author: `David Quick`,
   introduction: `Hi ✌️
-  I'm Dave, a developer from NY. I write web development articles for all. This is my journal to you. Enjoy!`,
+  I'm Dave, a developer from TX. I write web development articles for all. This is my journal to you. Enjoy!`,
   siteUrl: `https://papabearcodes.com`, // Your blog site url
   social: {
     twitter: `papabearcodes`, // Your Twitter account
@@ -23,5 +23,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'UA-134912381-1'
 }
